127b88c2612c845211528907d9c3b3263d242180
HH Veldstra <h@veldstra.org>
2015-01-30 11:25:37 +0000

    Is nice

--- discovery/index.js
+++ discovery/index.js
@@ -11,14 +11,17 @@ sock.bind(BROADCAST_PORT, function(err) {
   sock.send(bacon, 0, bacon.length, BROADCAST_PORT, '192.168.1.255');
 });
 
-
-
 sock.on('error', function(err) {
   console.error(err);
   sock.close()
 });
 
 sock.on('message', function(msg, rinfo) {
+  if(rinfo.address !== sock.address().address && msg !== 'is nice') {
+    var isNice = new Buffer('is nice');
+    sock.send(isNice, 0, isNice.length, BROADCAST_PORT, '192.168.1.255');
+  }
+
   console.log(msg.toString());
   console.log(rinfo);
 });
